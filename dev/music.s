458885d99aa4555011998a5064e4f300c394bc44
na_th_an <na_th_an@hotmail.com>
2016-12-27 19:14:11 +0100

    Reset MK1

--- dev/music.s
+++
@@ -1,9 +0,0 @@
-	.export _m_ingame
-	.export _m_title
-	.export _m_gover
-
-	.include "m_instruments.s"
-	
-_m_ingame:		.include "mus_game.s"
-_m_title:		.include "mus_title.s"
-_m_gover:		.include "mus_gover.s"
